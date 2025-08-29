5027eed10892225c4f4d48aebd1ec7fed59481fb
ln-one <ln1.opensource@gmail.com>
2025-08-30 01:10:27 +0800

    windows-clear

--- .chezmoitemplates/shell/powershell/shared_profile.ps1
+++ .chezmoitemplates/shell/powershell/shared_profile.ps1
@@ -7,21 +7,21 @@
 $moduleDir = "{{ .chezmoi.sourceDir }}\.chezmoitemplates\shell\powershell"
 
 # 00 - Environment variables and PATH setup
-{{ includeTemplate ".chezmoitemplates/shell/powershell/00-environment.ps1.tmpl" . | indent 0 }}
+{{ includeTemplate ".chezmoitemplates/shell/powershell/00-environment.ps1" . | indent 0 }}
 
 # 01 - Aliases and shortcuts
 $aliasFile = Join-Path $moduleDir "01-aliases.ps1"
 if (Test-Path $aliasFile) { . $aliasFile }
 
 # 02 - Custom functions
-{{ includeTemplate ".chezmoitemplates/shell/powershell/02-functions.ps1.tmpl" . | indent 0 }}
+{{ includeTemplate ".chezmoitemplates/shell/powershell/02-functions.ps1" . | indent 0 }}
 
 # 03 - Third-party tools initialization
 $toolsFile = Join-Path $moduleDir "03-tools.ps1"
 if (Test-Path $toolsFile) { . $toolsFile }
 
 # 04 - Secrets and 1Password integration
-{{ includeTemplate ".chezmoitemplates/shell/powershell/04-secrets.ps1.tmpl" . | indent 0 }}
+{{ includeTemplate ".chezmoitemplates/shell/powershell/04-secrets.ps1" . | indent 0 }}
 
 # Welcome message
 Write-Host "PowerShell environment loaded successfully" -ForegroundColor Green
