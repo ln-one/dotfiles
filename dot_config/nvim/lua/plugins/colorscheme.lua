270fc7821e8f04acd3c384459745857c083de1e4
ln-one <ln1.opensource@gmail.com>
2025-09-06 13:54:11 +0800

    nvim-auto-dark-mode

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,27 @@ return {
   -- 完全禁用 LazyVim 的默认 colorscheme 插件
   { "folke/tokyonight.nvim", enabled = false },
   
+  -- auto-dark-mode 插件，自动切换亮暗色主题
+  {
+    "f-person/auto-dark-mode.nvim",
+    lazy = false,
+    priority = 1500, -- 确保在 colorscheme 之前加载
+    config = function()
+      local auto_dark_mode = require("auto-dark-mode")
+      auto_dark_mode.setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.api.nvim_set_option_value("background", "dark", {})
+          vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option_value("background", "light", {})
+          vim.cmd.colorscheme("catppuccin-latte")
+        end,
+      })
+    end,
+  },
+  
   -- 重新定义 catppuccin 插件
   {
     "catppuccin/nvim",
@@ -10,7 +31,7 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
-        flavour = "macchiato",
+        flavour = "auto", -- 自动选择 flavour
         transparent_background = true,
         integrations = {
           blink_cmp = true,
@@ -28,11 +49,6 @@ return {
           bufferline = true,
         },
       })
-      
-      -- 延迟设置 colorscheme 避免冲突
-      vim.schedule(function()
-        vim.cmd.colorscheme("catppuccin-macchiato")
-      end)
     end,
   },
 }
